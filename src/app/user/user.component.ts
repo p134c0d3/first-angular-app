d96b338697152f58a56cfc4c1b863e92a7642737
p134c0d3 <70922864+p134c0d3@users.noreply.github.com>
2025-04-07 23:39:04 -0500

    Outputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,16 +6,22 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  // avatar = input.required<string>();
-  // name = input.required<string>();
+  // avatar = input.required<string>(); <-- this is the same as the above, newer, but not widely used
+  // name = input.required<string>(); <-- this is the same as the above, newer, but not widely used
+  // id = input.required<string>(); <-- this is the same as the above, newer, but not widely used
 
-  // imagePath = computed(() => `assets/users/${this.avatar()}`);
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>(); <-- this is the same as the above, newer, but not widely used
 
   get imagePath() {
     return `assets/users/${this.avatar}`;
   }
+  // imagePath = computed(() => `assets/users/${this.avatar()}`); <-- this is the same as the above, newer, but not widely used
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
